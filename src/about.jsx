65e9bd11a99c54db56056a47e30f1286824d1717
Aashii1826 <aashiagrawal1812@gmail.com>
2025-12-22 20:51:02 +0530

    Create about.jsx

---
+++ src/about.jsx
@@ -0,0 +1,33 @@
+function About() {
+  return (
+    <>
+      <div className="about">
+        <h2>Who Am I?</h2>
+        <p>
+          I’m Aashi, a travel enthusiast who documents destinations with a mix
+          of storytelling, photography, and practical travel guides.
+        </p>
+      </div>
+
+      <div className="about-2">
+        <h3>What You’ll Find Here:</h3>
+        <ul>
+          <li>Destination guides</li>
+          <li>Cultural stories</li>
+          <li>Budget travel tips</li>
+          <li>Best places to stay & eat</li>
+          <li>Personal experiences</li>
+        </ul>
+      </div>
+
+      <div className="about-3">
+        <p>
+          My goal is to help travelers explore smarter, travel deeper,
+          and appreciate the little details that make every place special.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default About;
