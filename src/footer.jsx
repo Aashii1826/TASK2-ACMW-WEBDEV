77679f834265a203008a85208560415b54a62b59
Aashii1826 <aashiagrawal1812@gmail.com>
2025-12-22 20:51:42 +0530

    Create footer.jsx

---
+++ src/footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+  return (
+    <>
+      <footer className="grid-3">
+        <div>
+          <h1>Treks and Trails</h1>
+          <p>Copyright © 2025</p>
+        </div>
+      </footer>
+
+      <div className="social">
+        <a href="#"><i className="fab fa-github fa-2x"></i></a>
+        <a href="#"><i className="fab fa-facebook fa-2x"></i></a>
+        <a href="#"><i className="fab fa-instagram fa-2x"></i></a>
+        <a href="#"><i className="fab fa-twitter fa-2x"></i></a>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
